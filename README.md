0317cadabbb9b69874945bca1893266280155fd7
sehrishilyas314 <sehrishilyas314@gmail.com>
2026-09-05 11:22:53 +0500

    add task to folder

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NAVTTC-Task
 This is my navttc task.
+<br> week1</br>
---
+++ task1.py
@@ -0,0 +1,5 @@
+
+x = ["alphabets", "numerics", "data types", "123", "wild" ,"abc", "chars"]
+if len(x) % 2 ==0:
+  print( "the length of list is even")
+  
